2118f7909c7030ca7b990c1b0dc5d12ba6f5cfc2
devanshu-jotwani <devanshujotwani14@gmail.com>
2022-04-19 19:56:17 +0530

    last push

--- app/static/app/js/prediction.js
+++ app/static/app/js/prediction.js
@@ -1,1 +1,8 @@
-console.log("hi")
+var pass=document.getElementById("dtr");
+var lr=document.getElementById("lr");
+
+var value=parseFloat(lr.innerHTML);
+console.log(value)
+if (value>6){
+    pass.innerHTML="PASS"
+} 
